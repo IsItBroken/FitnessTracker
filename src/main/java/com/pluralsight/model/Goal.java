fc134592ff791addca5a6f12994a17b4eae89008
Zachary Lipps <Zachary Lipps>
2017-02-16 22:11:54 -0500

    After validation

--- src/main/java/com/pluralsight/model/Goal.java
+++ src/main/java/com/pluralsight/model/Goal.java
@@ -1,10 +1,13 @@
 package com.pluralsight.model;
 
+import org.hibernate.validator.constraints.Range;
+
 /**
  * Created by zlipp on 2/16/2017.
  */
 public class Goal {
 
+    @Range(min = 1, max = 120)
     private int minutes;
 
     public int getMinutes() {
